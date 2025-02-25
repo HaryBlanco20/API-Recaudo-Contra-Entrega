802f65c5f17ad792e8faff3e2488be32a43bb61a
Ariadne Blanco <ariadneblanco20@gmail.com>
2025-02-25 09:07:43 -0500

    Eliminando archivos no necesarios

--- cucumber.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  default: `--require-module ts-node/register --require tests/stepsDefinitions/**/*.ts --format json:reports/cucumber_report.json --format json:reports/results.json --tags 'not @skip'`
-};
--- tests/tareas/Autenticarse.ts
+++
@@ -1,5 +0,0 @@
-import { ApiUser } from "../actores/ApiUser";
-
-export const Autenticarse = () => async (apiUser: ApiUser) => {
-  await apiUser.login();
-};
